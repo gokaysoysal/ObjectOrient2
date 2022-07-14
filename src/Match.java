72d769c4bc183f8d6b872a22906c223d12f56d0a
Gökay Soysal <agokaysoysal@gmail.com>
2022-07-14 16:03:00 +0300

    object orient

---
+++ src/Match.java
@@ -0,0 +1,57 @@
+public class Match {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    Match(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+
+    }
+
+    void run() {
+        if (isCheck()) {
+            while (this.f1.health > 0 && this.f2.health > 0) {
+                this.f2.health = this.f1.hit(this.f2);
+                if (isWin()){
+                    break;
+                }
+                this.f1.health = this.f2.hit(this.f1);
+                if (isWin()){
+                    break;
+                }
+                System.out.println(this.f2.name + " kalan saglik: " + this.f2.health);
+                System.out.println(this.f1.name + " kalan saglik: " + this.f1.health);;
+                System.out.println("-----------YENI ROUND----------");
+            }
+
+        } else {
+            System.out.println("Sporcularin sikletleri farkli!");
+        }
+    }
+
+
+    boolean isCheck() {
+        return (this.f1.weight >= minWeight && this.f1.weight <= maxWeight) && (this.f2.weight >= minWeight && this.f2.weight <= maxWeight);
+    }
+
+
+    boolean isWin() {
+        if (this.f1.health == 0) {
+            System.out.println(this.f2.name + " Kazandi!");
+            return true;
+        }
+        if (this.f2.health == 0) {
+            System.out.println(this.f1.name + " Kazandi!");
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+}
